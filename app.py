6eeb5394f35baa3d5a644c8754bc030405561d45
abineshm2104 <abism2104@gmail.com>
2024-12-10 11:35:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pymysql
 import pandas as pd
 
+# Creating Empty DataFrame and Storing it in variable df
+filtered_data = pd.DataFrame()
+
 # Connect to MySQL database
 def get_connection():
     return pymysql.connect(
@@ -9,22 +12,66 @@ def get_connection():
          password = '12345', database = 'guvi_project')
 
 # Function to fetch route names starting with a specific letter, arranged alphabetically
-def fetch_route_names(connection, starting_letter):
-    query = f"SELECT DISTINCT route_name FROM bus_routes WHERE route_name LIKE '{starting_letter}%' ORDER BY route_name"
+def fetch_route_names(connection):
+    query = f"SELECT DISTINCT route_name FROM bus_routes  ORDER BY route_name"
     route_names = pd.read_sql(query, connection)['route_name'].tolist()
-    return route_names
+    source_place=[]
+    destination_place=[]
+    if route_names:
+        for i in route_names:
+            source_place.append(i.split("to")[0].strip())
+            destination_place.append(i.split("to")[1].strip())
+        
+        source_place=list(set(source_place))
+        source_place.sort()
+        destination_place=list(set(destination_place))
+        destination_place.sort()
 
-# Function to fetch data from MySQL based on selected Route_Name and price sort order
-def fetch_data(connection, route_name, price_sort_order):
-    price_sort_order_sql = "ASC" if price_sort_order == "Low to High" else "DESC"
-    query = f"SELECT * FROM bus_routes WHERE route_name = %s ORDER BY start_rating DESC, price {price_sort_order_sql}"
-    df = pd.read_sql(query, connection, params=(route_name))
+    return source_place,destination_place
+
+# Function to fetch data from MySQL based on selected Route_Name 
+def fetch_data(connection,selected_source,selected_destination):
+    query = f"SELECT * FROM bus_routes WHERE route_name = %s ORDER BY star_rating DESC"
+    df = pd.read_sql(query, connection, params=(selected_source.strip()+" to "+selected_destination.strip()))
+    if len(df) >1:
+        df['departing_time'] =(df['departing_time'].dt.components[['hours','minutes','seconds']].astype(str).agg(':'.join,axis=1))
+        df['reaching_time'] = (df['reaching_time'].dt.components[['hours','minutes','seconds']].astype(str).agg(':'.join,axis=1))
     return df
 
-# Function to filter data based on Star_Rating and Bus_Type
-def filter_data(df, star_ratings, bus_types):
-    filtered_df = df[df['start_rating'].isin(star_ratings) & df['bustype'].isin(bus_types)]
-    return filtered_df
+#Function to filter based on rating
+def rating_filter_data(df,star_ratings):
+    rating_filtered_df=df[df['star_rating'].between(0.0,star_ratings)]
+    return rating_filtered_df
+#Function to filter based on bus type
+def bus_type_filter_data(df,bus_type):
+    bus_type_filter_df=df[df['bustype'].isin(bus_type)]    
+    return bus_type_filter_df
+#Function to filter based on price
+def Sort_by_Price(df, price_sort_order):
+    if price_sort_order =='Below 500':
+        price_filtered_df=df[df['price'] < 500 ]
+    elif price_sort_order=='500 to 1000':
+        price_filtered_df=df[df['price'].between(500, 1000)]
+    elif price_sort_order=='Above 1000':
+        price_filtered_df=df[df['price']>1000]
+    return price_filtered_df
+def time_order_filtered_data(df,time_order_filtered,column_name):
+    df[f"{column_name}"]=pd.to_datetime(df[f"{column_name}"],format='%H:%M:%S')
+    if time_order_filtered=="Before 6.am":
+        time_filtered_data =df[(df[f"{column_name}"]>=pd.to_datetime('00:00:00',format='%H:%M:%S')) & (df[f"{column_name}"]<=pd.to_datetime('05:59:59',format='%H:%M:%S'))]
+    elif time_order_filtered=='6am to 12pm':
+        time_filtered_data=df[(df[f"{column_name}"]>=pd.to_datetime('06:00:00',format='%H:%M:%S')) & (df[f"{column_name}"]<=pd.to_datetime('11:59:59',format='%H:%M:%S'))]
+    elif time_order_filtered=='12pm to 6pm':
+        time_filtered_data=df[(df[f"{column_name}"]>=pd.to_datetime('12:00:00',format='%H:%M:%S')) & (df[f"{column_name}"]<=pd.to_datetime('17:59:59',format='%H:%M:%S'))]
+    else:
+        time_filtered_data= df[(df[f"{column_name}"]>=pd.to_datetime('18:00:00',format='%H:%M:%S')) & (df[f"{column_name}"]<=pd.to_datetime('23:59:59',format='%H:%M:%S'))]
+    time_filtered_data[f"{column_name}"] =time_filtered_data[f"{column_name}"].dt.time
+    return time_filtered_data
+    #Function to filter based on seat available
+
+def seat_availability_list(df,seats_available):
+    seat_available_filtered_data=df[df['seats_available']==seats_available]
+    return seat_available_filtered_data
 
 # Main Streamlit app
 def main():
@@ -33,45 +80,76 @@ def main():
     connection = get_connection()
 
     try:
-        # Sidebar - Input for starting letter
-        starting_letter = st.sidebar.text_input('Enter starting letter of Route Name', '')
 
-        # Fetch route names starting with the specified letter
-        if starting_letter:
-            route_names = fetch_route_names(connection, starting_letter.upper())
+        source_place,destination_place= fetch_route_names(connection)        
+        # Sidebar - Input for source place
+        selected_source = st.sidebar.selectbox('Select source place', source_place,index=None,placeholder="enter source place")
+        if selected_source :
+            #destination_place.remove(selected_source)
+            # Sidebar - Input for destination place:
+            if selected_source in destination_place:
+                destination_place.remove(selected_source)
+                
+            selected_destination = st.sidebar.selectbox('Select destination place', destination_place,index=None,placeholder="enter destination place")
+                    
+            if selected_destination:
+                # Fetch data based on selected Route_Name
+                data = fetch_data(connection,selected_source,selected_destination)
+                
+                if not data.empty:
+                    # Display data table with a subheader
+                    st.write(f"### Data for Route: {selected_source} to {selected_destination}")
+                    st.write(data)
 
-            if route_names:
-                # Sidebar - Selectbox for Route_Name
-                selected_route = st.sidebar.radio('Select Route Name', route_names)
+                    rowcount=len(data)
+                    if rowcount >1:                            
+                        filtered_data =data;
 
-                if selected_route:
-                    # Sidebar - Selectbox for sorting preference
-                    price_sort_order = st.sidebar.selectbox('Sort by Price', ['Low to High', 'High to Low'])
+                        # Sidebar - Selectbox for sorting preference
+                        price_sort_order = st.sidebar.selectbox('Sort by Price', ['Below 500', '500 to 1000','Above 1000'],index=None,placeholder="Select price to filter")
+                        if price_sort_order:
+                            filtered_data =Sort_by_Price(filtered_data,price_sort_order)
+                        #Time
+                        time_order_filtered=st.sidebar.selectbox('Filter by Departing time', ['Before 6am', '6am to 12pm','12pm to 6pm','After 6pm'],index=None,placeholder="Select time to filter")
+                        if time_order_filtered:
+                            filtered_data = time_order_filtered_data(filtered_data,time_order_filtered,'departing_time')
 
-                    # Fetch data based on selected Route_Name and price sort order
-                    data = fetch_data(connection, selected_route, price_sort_order)
 
-                    if not data.empty:
-                        # Display data table with a subheader
-                        st.write(f"### Data for Route: {selected_route}")
-                        st.write(data)
+                        reaching_time_order_filtered=st.sidebar.selectbox('Filter by reaching_time', ['Before 6am', '6am to 12pm','12pm to 6pm','After 6pm'],index=None,placeholder="Select time to filter")
+                        if reaching_time_order_filtered:
+                            filtered_data = time_order_filtered_data(filtered_data,reaching_time_order_filtered,'reaching_time') 
+ 
 
+                         
+                            
                         # Filter by Star_Rating and Bus_Type
-                        star_ratings = data['start_rating'].unique().tolist()
-                        selected_ratings = st.multiselect('Filter by Star Rating', star_ratings)
+                        selected_ratings = st.sidebar.slider("Sort by Star Rating", min_value=0.0, max_value=5.0, value=0.0, step=0.1)
+                        if selected_ratings:
+                            filtered_data = rating_filter_data(filtered_data, selected_ratings)
+
+                        
 
                         bus_types = data['bustype'].unique().tolist()
-                        selected_bus_types = st.multiselect('Filter by Bus Type', bus_types)
+                        selected_bus_types = st.sidebar.multiselect('Filter by Bus Type', bus_types)
+                        if selected_bus_types:
+                            filtered_data = bus_type_filter_data(filtered_data,  selected_bus_types)
 
-                        if selected_ratings and selected_bus_types:
-                            filtered_data = filter_data(data, selected_ratings, selected_bus_types)
-                            # Display filtered data table with a subheader
-                            st.write(f"### Filtered Data for Star Rating: {selected_ratings} and Bus Type: {selected_bus_types}")
+                        seats= data['seats_available'].unique().tolist()
+                        seats_available=st.sidebar.selectbox('Enter by Seat Availability',seats,index=None,placeholder="Select seats_available to filter")
+                        if seats_available: 
+                            filtered_data =seat_availability_list(filtered_data,seats_available)
+                        
+                        if filtered_data.empty  :                            
+                            st.write(f"No data found for Route:{selected_source}  to {selected_destination}")
+                        elif filtered_data.equals(data):
+                            st.write(f"No data found for Route:{selected_source}  to {selected_destination}")
+                        else :
+                            st.write(f"### Filtered Data :")
                             st.write(filtered_data)
-                    else:
-                        st.write(f"No data found for Route: {selected_route} with the specified price sort order.")
+                else:
+                    st.write(f"No data found for Route:{selected_source}  to {selected_destination}")
             else:
-                st.write("No routes found starting with the specified letter.")
+                st.write("Please select destination place")
     finally:
         connection.close()
 
